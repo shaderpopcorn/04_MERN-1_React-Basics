fbd41718ce82d9734857dcfb734107940f47aa02
shaderpopcorn <kwmail@gmx.de>
2023-09-26 12:01:01 +0200

    select sorted

--- src/App.jsx
+++ src/App.jsx
@@ -7,45 +7,52 @@ function App() {
   const [date, setDate] = useState(today);
   const [apodData, setApodData] = useState(null);
   const [marsData, setMarsData] = useState(null);
+  const [select, setSelect] = useState("apod");
 
   useEffect(() => {
-    const apodFetch = async () => {
-      try {
-        const res = await fetch(
-          `${import.meta.env.VITE_NASA_URL}/planetary/apod?api_key=${
-            import.meta.env.VITE_API
-          }&date=${date}`
-        );
-        const data = await res.json();
-        setApodData(data);
-        console.log(data);
-      } catch (error) {
-        console.error("Not able to fetch APOD data", error);
-      }
-    };
-    apodFetch();
+    if (select === "apod") {
+      (async () => {
+        try {
+          const res = await fetch(
+            `${import.meta.env.VITE_NASA_URL}/planetary/apod?api_key=${
+              import.meta.env.VITE_API
+            }&date=${date}`
+          );
+          const data = await res.json();
+          setApodData(data);
+          console.log(data);
+        } catch (error) {
+          console.error("Not able to fetch APOD data", error);
+        }
+      })();
+    }
 
-    const marsFetch = async () => {
-      try {
-        const res = await fetch(
-          `${
-            import.meta.env.VITE_NASA_URL
-          }/mars-photos/api/v1/rovers/curiosity/photos?api_key=${
-            import.meta.env.VITE_API
-          }&earth_date=${date}`
-        );
-        const data = await res.json();
-        setMarsData(data);
-        console.log(data.photos[0]);
-      } catch (error) {
-        console.error("Not able to fetch APOD data", error);
-      }
-    };
-    marsFetch();
-  }, [date]);
+    if (select === "mars") {
+      (async () => {
+        try {
+          const res = await fetch(
+            `${
+              import.meta.env.VITE_NASA_URL
+            }/mars-photos/api/v1/rovers/curiosity/photos?api_key=${
+              import.meta.env.VITE_API
+            }&earth_date=${date}`
+          );
+          const data = await res.json();
+          setMarsData(data);
+          console.log(data.photos[0]);
+        } catch (error) {
+          console.error("Not able to fetch APOD data", error);
+        }
+      })();
+    }
+  }, [date, select]);
 
   const handleInput = (ev) => {
     setDate(ev.target.value);
+  };
+
+  const handleSelect = (ev) => {
+    setSelect(ev.target.value);
   };
 
   return (
@@ -54,8 +61,10 @@ function App() {
       <form action="">
         <input type="date" value={date} onChange={handleInput} />
         <br />
-        <select name="planet-select" id="planet-select">
-          <option value="apod">APOD</option>
+        <select name="planet-select" id="planet-select" onInput={handleSelect}>
+          <option selected="selected" value="apod">
+            APOD
+          </option>
           <option value="mars">MARS</option>
         </select>
       </form>
